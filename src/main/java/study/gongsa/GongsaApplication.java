64faca90180b4143d5a20415e24528bd41572a32
hailie <20181011@sungshin.ac.kr>
2022-08-06 22:47:32 +0900

    Initial Commit

---
+++ src/main/java/study/gongsa/GongsaApplication.java
@@ -0,0 +1,13 @@
+package study.gongsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/gongsa/GongsaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.gongsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
