aaf732e95ae7a61caf0af89538e61044b934b125
hailie <20181011@sungshin.ac.kr>
2022-08-07 01:51:30 +0900

    feat: 회원가입에 이메일 인증코드 전송 기능 추가

--- src/main/java/study/gongsa/service/UserService.java
+++ src/main/java/study/gongsa/service/UserService.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import study.gongsa.domain.User;
+import study.gongsa.dto.MailDto;
 import study.gongsa.repository.UserRepository;
 import study.gongsa.support.GenerateAuthCode;
+import study.gongsa.support.mail.MailService;
 
 import java.util.Date;
 import java.util.Optional;
@@ -15,11 +17,13 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final MailService mailService;
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, MailService mailService){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.mailService = mailService;
     }
 
     public Number join(User user){
@@ -46,6 +50,9 @@ public class UserService {
         user.setAuthCode(authCode);
 
         //이메일 전송
+        MailDto mailDto = new MailDto();
+        mailDto.setRegisterMailForm(user.getEmail(),user.getNickname(),user.getAuthCode());
+        mailService.sendMail(mailDto);
 
         Date currentTime = new Date();
         user.setCreatedAt(currentTime);
