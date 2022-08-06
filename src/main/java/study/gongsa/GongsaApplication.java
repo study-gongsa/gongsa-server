d08358e4c8125af0c5c0259306d79615553eaa5a
hailie <20181011@sungshin.ac.kr>
2022-08-07 00:09:08 +0900

    fix: timezone 변경
    createdAt, updatedAt을 위한 timezone 변경

--- src/main/java/study/gongsa/GongsaApplication.java
+++ src/main/java/study/gongsa/GongsaApplication.java
@@ -2,9 +2,16 @@ package study.gongsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class GongsaApplication {
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GongsaApplication.class, args);
