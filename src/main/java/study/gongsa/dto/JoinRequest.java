e974dddc8af911a85b578354c27f62281295b95e
hailie <20181011@sungshin.ac.kr>
2022-08-07 22:49:13 +0900

    fix: 회원가입 요청값 검증
    validation 사용하여 post /api/user reqeust 값 검증

--- src/main/java/study/gongsa/dto/JoinRequest.java
+++ src/main/java/study/gongsa/dto/JoinRequest.java
@@ -2,17 +2,24 @@ package study.gongsa.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class JoinRequest {
-    @NotBlank(message = "이메일은 필수값 입니다.")
+    @NotBlank(message = "이메일은 필수값 입니다")
+    @Email
     String email;
+
     @NotBlank(message = "비밀번호는 필수값 입니다.")
+    @Size(min = 8, max = 16, message = "비밀번호는 8~16자여야 합니다")
     String passwd;
+
     @NotBlank(message = "닉네임은 필수값 입니다.")
+    @Size(min = 1, max = 10, message = "닉넹님은 1~10자여야 합니다")
     String nickname;
 }
