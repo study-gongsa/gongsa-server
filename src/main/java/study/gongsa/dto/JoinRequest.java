6f7b89f7dff054abcd7b64794fb7e70c5b50f95e
hailie <20181011@sungshin.ac.kr>
2022-08-08 01:37:02 +0900

    refactor: 회원가입 request validation 오류 메시지 변경

--- src/main/java/study/gongsa/dto/JoinRequest.java
+++ src/main/java/study/gongsa/dto/JoinRequest.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 public class JoinRequest {
     @NotBlank(message = "이메일은 필수값 입니다")
-    @Email
+    @Email(message = "이메일 형식이 맞지 않습니다.")
     String email;
 
     @NotBlank(message = "비밀번호는 필수값 입니다.")
@@ -20,6 +20,6 @@ public class JoinRequest {
     String passwd;
 
     @NotBlank(message = "닉네임은 필수값 입니다.")
-    @Size(min = 1, max = 10, message = "닉넹님은 1~10자여야 합니다")
+    @Size(min = 1, max = 10, message = "닉네임은 1~10자여야 합니다")
     String nickname;
 }
