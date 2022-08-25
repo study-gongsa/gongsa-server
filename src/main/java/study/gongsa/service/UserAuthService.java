a5e45f4c1d336256d4776444e653b15c4b67d3d8
hailie <20181011@sungshin.ac.kr>
2022-08-25 14:57:21 +0900

    feat: refreshToken 확인하는 기능 추가
    db에 저장된 refreshToken과 일치하는지 확인

--- src/main/java/study/gongsa/service/UserAuthService.java
+++ src/main/java/study/gongsa/service/UserAuthService.java
@@ -1,9 +1,14 @@
 package study.gongsa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import study.gongsa.domain.User;
 import study.gongsa.domain.UserAuth;
 import study.gongsa.repository.UserAuthRepository;
+import study.gongsa.support.exception.IllegalStateExceptionWithLocation;
+
+import java.util.Optional;
 
 @Service
 public class UserAuthService {
@@ -17,5 +22,16 @@ public class UserAuthService {
 
     public void save(UserAuth userAuth){
         userAuthRepository.save(userAuth);
+    }
+
+    public void checkRefreshToken(int userUID, String refreshToken){
+        Optional<UserAuth> userAuthByUserUID = userAuthRepository.findByUserUID(userUID);
+        if(userAuthByUserUID.isEmpty())
+            throw new IllegalStateExceptionWithLocation(HttpStatus.UNAUTHORIZED, "email","가입되지 않은 회원입니다.");
+        System.out.println("1."+ userAuthByUserUID.get().getRefreshToken());
+        System.out.println("2."+ refreshToken);
+        if(!refreshToken.equals(userAuthByUserUID.get().getRefreshToken())){ //일치X
+            throw new IllegalStateExceptionWithLocation(HttpStatus.BAD_REQUEST, "", "올바르지 않은 refresh token입니다.");
+        }
     }
 }
