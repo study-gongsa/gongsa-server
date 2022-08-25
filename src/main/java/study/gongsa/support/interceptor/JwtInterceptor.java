b22a62c1fe4671bb17c89e15e44f4c74fbd505be
hailie <20181011@sungshin.ac.kr>
2022-08-25 14:55:56 +0900

    feat: /refresh 만료된 accessToken일 경우 추가

--- src/main/java/study/gongsa/support/interceptor/JwtInterceptor.java
+++ src/main/java/study/gongsa/support/interceptor/JwtInterceptor.java
@@ -1,6 +1,7 @@
 package study.gongsa.support.interceptor;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -28,8 +29,9 @@ public class JwtInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String headerToken = null;
         try{
-            String headerToken = request.getHeader("Authorization");
+            headerToken = request.getHeader("Authorization");
             if (headerToken == null)
                 throw new NullPointerException();
 
@@ -37,10 +39,18 @@ public class JwtInterceptor implements HandlerInterceptor {
             int userUID = (int)data.get("userUID");
             if(!userService.isAuth(userUID))
                 throw new IllegalArgumentException("notAuth");
-
             request.setAttribute("userUID", userUID);
 
             return true;
+        } catch(ExpiredJwtException e){
+            Boolean isRefresh = ("POST".equals(request.getMethod())) && ("/api/user/login/refresh".equals(request.getRequestURI()));
+            if(isRefresh){
+                // decode
+                int userUID = jwtTokenProvider.decode(headerToken);
+                request.setAttribute("userUID", userUID);
+                return true;
+            }
+            throw new IllegalStateExceptionWithLocation(HttpStatus.UNAUTHORIZED,"auth", "로그인 후 이용해주세요.");
         } catch(IllegalArgumentException e){
             System.out.println(e.getMessage());
             if(e.getMessage().equals("notAuth"))
@@ -48,7 +58,7 @@ public class JwtInterceptor implements HandlerInterceptor {
             else
                 throw new IllegalStateExceptionWithLocation(HttpStatus.UNAUTHORIZED,"auth", "로그인 후 이용해주세요.");
         } catch (Exception e){
-            System.out.println("message: " + e.getMessage());
+            System.out.println(e.getClass() + " message: " + e.getMessage());
             throw new IllegalStateExceptionWithLocation(HttpStatus.UNAUTHORIZED,"auth", "로그인 후 이용해주세요.");
         }
     }
