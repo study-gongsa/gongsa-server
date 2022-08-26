b0b79511f77aaf32a158a802b60f47880aebf4a3
hailie <20181011@sungshin.ac.kr>
2022-08-26 16:24:05 +0900

    feat: studyGroup의 최소 공부시간 합이 24시간을 넘지 않는지 체크
    유저가 가입한 스터디 그룹 최소 공부 시간과 새로 가입할 스터디 그룹 최소 공부 시간 합이 24시간을 넘지 않는지 체크하는 기능 추가

--- src/main/java/study/gongsa/service/StudyGroupService.java
+++ src/main/java/study/gongsa/service/StudyGroupService.java
@@ -1,12 +1,16 @@
 package study.gongsa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import study.gongsa.domain.StudyGroup;
 import study.gongsa.repository.StudyGroupRepository;
 import study.gongsa.repository.UserCategoryRepository;
+import study.gongsa.support.exception.IllegalStateExceptionWithLocation;
 
+import java.sql.Time;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudyGroupService {
@@ -29,5 +33,22 @@ public class StudyGroupService {
 
     public List<StudyGroup> findSameCategoryAllByUserUID(int userUID){
         return studyGroupRepository.findSameCategoryAllByUserUID(userUID);
+    }
+
+    public void checkPossibleMinStudyHourByUsersUID(int userUID, int addedMinStudyHour){
+        Optional<Integer> userSumMinStudyHour = studyGroupRepository.findSumMinStudyHourByUserUID(userUID);
+        int currentUserSumMinStudyHour = 0;
+        if(userSumMinStudyHour.isPresent()) currentUserSumMinStudyHour = userSumMinStudyHour.get();
+        if(currentUserSumMinStudyHour+addedMinStudyHour > 24){
+            throw new IllegalStateExceptionWithLocation(HttpStatus.BAD_REQUEST,"minStudyHour","하루 최소 공부시간 합이 24시간을 초과합니다.");
+        }
+    }
+
+    public int makeStudyGroup(StudyGroup studyGroup, int[] groupCategories) {
+        //그룹, 카테고리 생성
+        return 0;
+    }
+
+    public void makeStudyGroupMember(int groupUID, int userUID, boolean isLeader) {
     }
 }
