ca05c7fa4dcf4089355fdb69955cd5a6b8d7736e
hailie <59546976+DeveloperHailie@users.noreply.github.com>
2022-10-05 13:47:56 +0900

    chore: getImage swagger 추가

--- src/main/java/study/gongsa/controller/ImageController.java
+++ src/main/java/study/gongsa/controller/ImageController.java
@@ -1,6 +1,9 @@
 package study.gongsa.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -26,6 +29,11 @@ public class ImageController {
         this.imageService = imageService;
     }
 
+    @ApiOperation(value="이미지 얻기")
+    @ApiResponses({
+            @ApiResponse(code=200, message="이미지 반환"),
+            @ApiResponse(code=400, message="이미지를 불러올 수 없습니다."),
+    })
     @GetMapping("/{imageName:.+}")
     public ResponseEntity<Resource> getImage(@PathVariable String imageName) {
         System.out.println(imageName);
