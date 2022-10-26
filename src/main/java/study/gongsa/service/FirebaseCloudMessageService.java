d8e9d44659f566191c42ef7d94f510e35cc99ebc
hailie <59546976+DeveloperHailie@users.noreply.github.com>
2022-10-26 18:07:39 +0900

    feat: fcm 관련 서비스
    1.  FCM에 push 요청 시 Header에 사용할 Access Token 받아오기 2. push 알림 전송 및 메시지 만들기

---
+++ src/main/java/study/gongsa/service/FirebaseCloudMessageService.java
@@ -0,0 +1,78 @@
+package study.gongsa.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+import study.gongsa.dto.FcmMessage;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.*;
+
+@Service
+public class FirebaseCloudMessageService {
+
+    @Value("${fcm.url}")
+    private String API_URL;
+    @Value("${fcm.key}")
+    private String firebaseConfigPath;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    // FCM에 push 요청 시 Header에 사용할 Access Token 받아오기
+    private String getAccessToken() throws IOException {
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new
+                        ClassPathResource(firebaseConfigPath).getInputStream())
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+
+        googleCredentials.refreshIfExpired();
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+
+    public void sendMessageTo(String targetToken, String title, String body)  {
+        try{
+            String message = makeMessage(targetToken, title, body);
+
+            OkHttpClient client = new OkHttpClient();
+            RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+            Request request = new Request.Builder()
+                    .url(API_URL)
+                    .post(requestBody)
+                    .addHeader(AUTHORIZATION, "Bearer " + getAccessToken())
+                    .addHeader(CONTENT_TYPE, "application/json; UTF-8")
+                    .build();
+            Response response = client.newCall(request)
+                    .execute();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private String makeMessage(String targetToken, String title, String body) {
+        try{
+            FcmMessage fcmMessage = FcmMessage.builder()
+                    .message(FcmMessage.Message.builder()
+                            .token(targetToken)
+                            .notification(FcmMessage.Notification.builder()
+                                    .title(title)
+                                    .body(body)
+                                    .image(null)
+                                    .build()
+                            )
+                            .build()
+                    )
+                    .validate_only(false)
+                    .build();
+
+            return objectMapper.writeValueAsString(fcmMessage);
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
