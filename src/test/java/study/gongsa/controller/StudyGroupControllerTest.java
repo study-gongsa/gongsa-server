1cd6852f012628f86fa9acf16c03829a6b7b756f
hailie <20181011@sungshin.ac.kr>
2022-11-02 20:13:01 +0900

    test: 스터디그룹 코드 조회, 내 스터디그룹 조회 테스트 코드

--- src/test/java/study/gongsa/controller/StudyGroupControllerTest.java
+++ src/test/java/study/gongsa/controller/StudyGroupControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,11 +22,14 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import study.gongsa.domain.GroupMember;
 import study.gongsa.domain.StudyGroup;
 import study.gongsa.domain.User;
 import study.gongsa.domain.UserAuth;
 import study.gongsa.dto.DefaultResponse;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import study.gongsa.dto.MakeStudyGroupRequest;
+import study.gongsa.repository.GroupMemberRepository;
 import study.gongsa.repository.StudyGroupRepository;
 import study.gongsa.repository.UserAuthRepository;
 import study.gongsa.repository.UserRepository;
@@ -36,9 +40,10 @@ import java.io.FileInputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Date;
+import java.sql.Time;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -50,7 +55,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class StudyGroupControllerTest {
 
     private static String baseURL = "/api/study-group";
-    private Integer userUID;
+    private Integer userUID, leaderUserUID, memberUserUID;
+    private Integer groupUID;
     private String accessToken;
     private Integer madeGroupUID = 0;
 
@@ -66,6 +72,8 @@ class StudyGroupControllerTest {
     private UserAuthRepository userAuthRepository;
     @Autowired
     private StudyGroupRepository studyGroupRepository;
+    @Autowired
+    private GroupMemberRepository groupMemberRepository;
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
     @Autowired
@@ -93,6 +101,57 @@ class StudyGroupControllerTest {
                 .refreshToken(jwtTokenProvider.makeRefreshToken(userUID))
                 .build()).intValue();
         accessToken = jwtTokenProvider.makeAccessToken(userUID, userAuthUID);
+
+        // 스터디 그룹 멤버
+        User leader = User.builder()
+                .email("gong40sa04_@gmail.com")
+                .passwd(passwordEncoder.encode("12345678"))
+                .nickname("통합테스트_리더")
+                .authCode("00000b")
+                .build();
+        leader.setIsAuth(true);
+        leaderUserUID = userRepository.save(leader).intValue();
+        User member = User.builder()
+                .email("gong40sa04_2@gmail.com")
+                .passwd(passwordEncoder.encode("12345678"))
+                .nickname("통합테스트_멤버")
+                .authCode("00000c")
+                .build();
+        member.setIsAuth(true);
+        memberUserUID = userRepository.save(member).intValue();
+
+        // 스터디 그룹 생성 및 멤버들 가입
+        StudyGroup studyGroup = StudyGroup.builder()
+                .name("test_group")
+                .code("0000000000000000")
+                .isCam(true)
+                .isPrivate(false)
+                .minStudyHour(Time.valueOf("23:00:00"))
+                .maxMember(4)
+                .maxTodayStudy(6)
+                .isPenalty(true)
+                .maxPenalty(6)
+                .expiredAt(Date.valueOf("2023-10-10"))
+                .build();
+        groupUID = studyGroupRepository.save(studyGroup).intValue();
+        GroupMember groupLeader = GroupMember.builder()
+                .userUID(leaderUserUID)
+                .groupUID(groupUID)
+                .isLeader(true)
+                .build();
+        GroupMember groupMember = GroupMember.builder()
+                .userUID(memberUserUID)
+                .groupUID(groupUID)
+                .isLeader(false)
+                .build();
+        GroupMember userMember = GroupMember.builder()
+                .userUID(userUID)
+                .groupUID(groupUID)
+                .isLeader(false)
+                .build();
+        groupMemberRepository.save(groupLeader);
+        groupMemberRepository.save(groupMember);
+        groupMemberRepository.save(userMember);
     }
 
     @AfterEach
@@ -113,7 +172,52 @@ class StudyGroupControllerTest {
     }
 
     @Test
-    void testFindOneByUID() {
+    void 코드로스터디그룹조회_성공() throws Exception{
+        // given
+        StudyGroup studyGroup = studyGroupRepository.findByUID(groupUID).get();
+
+        // when
+        ResultActions resultActions = mockMvc.perform(get(baseURL+"/code/"+studyGroup.getCode())
+                        .header("Authorization", "Bearer "+accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print());
+
+        // then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.groupUID").value(studyGroup.getUID()))
+                .andExpect(jsonPath("$.data.name").value(studyGroup.getName()))
+                .andExpect(jsonPath("$.data.code").value(studyGroup.getCode()))
+                .andExpect(jsonPath("$.data.isCam").value(studyGroup.getIsCam()))
+                .andExpect(jsonPath("$.data.minStudyHour").value(studyGroup.getMinStudyHour().toString()))
+                .andExpect(jsonPath("$.data.expiredAt").exists())
+                .andExpect(jsonPath("$.data.createdAt").exists())
+                .andExpect(jsonPath("$.data.categories").exists());
+    }
+
+    @Test
+    void 나의스터디그룹조회_성공() throws Exception {
+        // given
+        StudyGroup studyGroup = studyGroupRepository.findByUID(groupUID).get();
+
+        // when
+        ResultActions resultActions = mockMvc.perform(get(baseURL+"/my-group")
+                        .header("Authorization", "Bearer "+accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print());
+
+        // then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.studyGroupList.length()").value(1))
+                .andExpect(jsonPath("$.data.studyGroupList[0].studyGroupUID").value(groupUID))
+                .andExpect(jsonPath("$.data.studyGroupList[0].imgPath").exists())
+                .andExpect(jsonPath("$.data.studyGroupList[0].name").value(studyGroup.getName()))
+                .andExpect(jsonPath("$.data.studyGroupList[0].isCam").value(studyGroup.getIsCam()))
+                .andExpect(jsonPath("$.data.studyGroupList[0].createdAt").exists())
+                .andExpect(jsonPath("$.data.studyGroupList[0].expiredAt").exists());
     }
 
     @Test
