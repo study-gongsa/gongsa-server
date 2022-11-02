b022526c9e3d6b10f2d03ad3e7eb732e35503c8e
hailie <59546976+DeveloperHailie@users.noreply.github.com>
2022-11-02 17:46:36 +0900

    feat: Answer 찾기, 수정 db

--- src/main/java/study/gongsa/repository/AnswerRepository.java
+++ src/main/java/study/gongsa/repository/AnswerRepository.java
@@ -5,8 +5,13 @@ import study.gongsa.domain.AnswerInfo;
 import study.gongsa.domain.Question;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AnswerRepository {
     List<AnswerInfo> findAnswer(int questionUID);
     Number save(Answer answer);
+
+    Optional<Answer> findOne(int UID);
+
+    void update(int UID, String content);
 }
