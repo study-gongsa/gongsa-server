c62dd487c07638280b980d6823c86b53c40a78c8
hailie <59546976+DeveloperHailie@users.noreply.github.com>
2022-11-03 11:53:43 +0900

    fix: GroupCategory 시간에 디폴트값 설정

--- src/main/java/study/gongsa/domain/GroupCategory.java
+++ src/main/java/study/gongsa/domain/GroupCategory.java
@@ -14,9 +14,10 @@ public class GroupCategory {
     private int UID;
     private int groupUID;
     private int categoryUID;
-    private Timestamp createdAt;
-    private Timestamp updatedAt;
-
+    @Builder.Default
+    private Timestamp createdAt = new Timestamp(new java.util.Date().getTime());
+    @Builder.Default
+    private Timestamp updatedAt = new Timestamp(new java.util.Date().getTime());
     public GroupCategory(int groupUID){
         this.groupUID = groupUID;
     }
